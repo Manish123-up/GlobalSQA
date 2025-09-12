bc8d73d83adc2e88c6d455e08ef405fb80316c2f
Manish Upadhyay <upmanish354@gmail.com>
2025-09-12 19:01:47 +0530

    commented a line in constant

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -2,5 +2,5 @@ package constants;
 
 public class Constants {
     public final static String HOME_PAGE = "home-page";
-    public final static String DATE_PICKER = "date-picker-page";
+    //public final static String DATE_PICKER = "date-picker-page";
 }
