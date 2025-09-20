9ff75ef2e19d0250bf84b92214c158619e131edf
laxmi kant joshi <lkjoshi@laxmis-MacBook-Pro.local>
2025-09-20 09:53:58 +0530

    tooltip features

--- src/main/java/pages/ToolTipPage.java
+++ src/main/java/pages/ToolTipPage.java
@@ -3,7 +3,6 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
@@ -20,18 +19,28 @@ public class ToolTipPage extends BasePage {
         return heading.getText();
     }
 
-    public String getToolTipText() {
+    public String getToolTipTextForImageBasedTab() {
         WebElement toolTipFrame1 = driver.findElement(By.cssSelector("[src*='tooltip/custom-content']"));
         driver.switchTo().frame(toolTipFrame1);
         Actions actions = new Actions(driver);
         List<WebElement> citiesNames = driver.findElements(By.cssSelector("[class='ui-widget-header ui-corner-all']>h3>a"));
         actions.moveToElement(citiesNames.get(0)).perform();
-        try {
-            Thread.sleep(3000);
-        } catch (Exception e) {
-
-        }
         List<WebElement> city = driver.findElements(By.cssSelector("[class='ui-tooltip-content']>img"));
         return city.get(0).getAttribute("alt");
+    }
+
+    public void clickOnAToolTipTab() {
+        WebElement element = driver.findElement(By.cssSelector("[id*='Forms Based']"));
+        element.click();
+    }
+
+    public String getToolTipTextForFormBasedTab() {
+        WebElement toolTipFrame2 = driver.findElement(By.cssSelector("[src*='tooltip/forms.html']"));
+        driver.switchTo().frame(toolTipFrame2);
+        Actions actions = new Actions(driver);
+        WebElement firstName = driver.findElement(By.cssSelector("[id='firstname']"));
+        actions.moveToElement(firstName).perform();
+        WebElement firstNameToolTip = driver.findElement(By.cssSelector(".ui-tooltip-content"));
+        return firstNameToolTip.getText();
     }
 }
